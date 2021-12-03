0c98b357e152fcdafd34f5c4cf024c5e187693c6
rnfmfz <95463368+rnfmfz@users.noreply.github.com>
2021-12-02 23:55:09 -0600

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿
+namespace TheWalkingDeadShootingGalleryPrototype
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.upperLeftPictureBox = new System.Windows.Forms.PictureBox();
+            this.middleLeftPictureBox = new System.Windows.Forms.PictureBox();
+            this.upperRightPictureBox = new System.Windows.Forms.PictureBox();
+            this.lowerRightPictureBox = new System.Windows.Forms.PictureBox();
+            this.lowerMiddlePictureBox = new System.Windows.Forms.PictureBox();
+            this.middleMiddlePictureBox = new System.Windows.Forms.PictureBox();
+            this.upperMiddlePictureBox = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.upperLeftPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.middleLeftPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.upperRightPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lowerRightPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lowerMiddlePictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.middleMiddlePictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.upperMiddlePictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // upperLeftPictureBox
+            // 
+            this.upperLeftPictureBox.BackgroundImage = global::TheWalkingDeadShootingGalleryPrototype.Properties.Resources.Glenn;
+            this.upperLeftPictureBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.upperLeftPictureBox.Location = new System.Drawing.Point(149, 26);
+            this.upperLeftPictureBox.Name = "upperLeftPictureBox";
+            this.upperLeftPictureBox.Size = new System.Drawing.Size(109, 79);
+            this.upperLeftPictureBox.TabIndex = 0;
+            this.upperLeftPictureBox.TabStop = false;
+            // 
+            // middleLeftPictureBox
+            // 
+            this.middleLeftPictureBox.BackgroundImage = global::TheWalkingDeadShootingGalleryPrototype.Properties.Resources.Daryl;
+            this.middleLeftPictureBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.middleLeftPictureBox.Location = new System.Drawing.Point(80, 194);
+            this.middleLeftPictureBox.Name = "middleLeftPictureBox";
+            this.middleLeftPictureBox.Size = new System.Drawing.Size(109, 79);
+            this.middleLeftPictureBox.TabIndex = 1;
+            this.middleLeftPictureBox.TabStop = false;
+            // 
+            // upperRightPictureBox
+            // 
+            this.upperRightPictureBox.BackgroundImage = global::TheWalkingDeadShootingGalleryPrototype.Properties.Resources.Rick;
+            this.upperRightPictureBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.upperRightPictureBox.Location = new System.Drawing.Point(838, 26);
+            this.upperRightPictureBox.Name = "upperRightPictureBox";
+            this.upperRightPictureBox.Size = new System.Drawing.Size(109, 79);
+            this.upperRightPictureBox.TabIndex = 2;
+            this.upperRightPictureBox.TabStop = false;
+            this.upperRightPictureBox.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // lowerRightPictureBox
+            // 
+            this.lowerRightPictureBox.BackgroundImage = global::TheWalkingDeadShootingGalleryPrototype.Properties.Resources.Negan;
+            this.lowerRightPictureBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.lowerRightPictureBox.Location = new System.Drawing.Point(871, 288);
+            this.lowerRightPictureBox.Name = "lowerRightPictureBox";
+            this.lowerRightPictureBox.Size = new System.Drawing.Size(109, 79);
+            this.lowerRightPictureBox.TabIndex = 3;
+            this.lowerRightPictureBox.TabStop = false;
+            // 
+            // lowerMiddlePictureBox
+            // 
+            this.lowerMiddlePictureBox.BackgroundImage = global::TheWalkingDeadShootingGalleryPrototype.Properties.Resources.The_Governor;
+            this.lowerMiddlePictureBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.lowerMiddlePictureBox.Location = new System.Drawing.Point(441, 353);
+            this.lowerMiddlePictureBox.Name = "lowerMiddlePictureBox";
+            this.lowerMiddlePictureBox.Size = new System.Drawing.Size(109, 79);
+            this.lowerMiddlePictureBox.TabIndex = 4;
+            this.lowerMiddlePictureBox.TabStop = false;
+            // 
+            // middleMiddlePictureBox
+            // 
+            this.middleMiddlePictureBox.BackgroundImage = global::TheWalkingDeadShootingGalleryPrototype.Properties.Resources.Carl;
+            this.middleMiddlePictureBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.middleMiddlePictureBox.Location = new System.Drawing.Point(441, 194);
+            this.middleMiddlePictureBox.Name = "middleMiddlePictureBox";
+            this.middleMiddlePictureBox.Size = new System.Drawing.Size(109, 79);
+            this.middleMiddlePictureBox.TabIndex = 5;
+            this.middleMiddlePictureBox.TabStop = false;
+            // 
+            // upperMiddlePictureBox
+            // 
+            this.upperMiddlePictureBox.BackgroundImage = global::TheWalkingDeadShootingGalleryPrototype.Properties.Resources.Maggie;
+            this.upperMiddlePictureBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.upperMiddlePictureBox.Location = new System.Drawing.Point(546, 39);
+            this.upperMiddlePictureBox.Name = "upperMiddlePictureBox";
+            this.upperMiddlePictureBox.Size = new System.Drawing.Size(109, 79);
+            this.upperMiddlePictureBox.TabIndex = 6;
+            this.upperMiddlePictureBox.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 511);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1082, 110);
+            this.panel1.TabIndex = 7;
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::TheWalkingDeadShootingGalleryPrototype.Properties.Resources.Prison;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1082, 621);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.upperMiddlePictureBox);
+            this.Controls.Add(this.middleMiddlePictureBox);
+            this.Controls.Add(this.lowerMiddlePictureBox);
+            this.Controls.Add(this.lowerRightPictureBox);
+            this.Controls.Add(this.upperRightPictureBox);
+            this.Controls.Add(this.middleLeftPictureBox);
+            this.Controls.Add(this.upperLeftPictureBox);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "mainForm";
+            this.Text = "The Walking Dead Shooting Gallery Prototype";
+            ((System.ComponentModel.ISupportInitialize)(this.upperLeftPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.middleLeftPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.upperRightPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lowerRightPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lowerMiddlePictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.middleMiddlePictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.upperMiddlePictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox upperLeftPictureBox;
+        private System.Windows.Forms.PictureBox middleLeftPictureBox;
+        private System.Windows.Forms.PictureBox upperRightPictureBox;
+        private System.Windows.Forms.PictureBox lowerRightPictureBox;
+        private System.Windows.Forms.PictureBox lowerMiddlePictureBox;
+        private System.Windows.Forms.PictureBox middleMiddlePictureBox;
+        private System.Windows.Forms.PictureBox upperMiddlePictureBox;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
